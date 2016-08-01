853eeb8545129445917a732cf6c55e3bd757958f
Abhishek Malali <abhishekmalali@gmail.com>
2016-08-01 15:11:00 -0700

    Adding test to check parameter addition and removal

--- h2ohyperopt/tests/test_modelOpt_parameters.py
+++ h2ohyperopt/tests/test_modelOpt_parameters.py
@@ -14,3 +14,22 @@ class TestModelOptParameters(TestCase):
         model.model_parameters_opt(return_dict=False)
         params_list = model.model_parameters_opt(return_dict=True)
         assert params_list == model.model_params
+
+    def testSelOptParameters(self):
+        model = h2ohyperopt.ModelOptimizer(metric='auc')
+        model.select_optimization_parameters(params=
+                                        {'ntrees': ('choice', [10, 20, 30])})
+        assert set(model.model_params.keys()) == set(['metric', 'ntrees'])
+
+    def testAddRemOptParameters(self):
+        model = h2ohyperopt.ModelOptimizer(metric='auc')
+        model.select_optimization_parameters(params="Default")
+        model.add_optimization_parameters({'col_sample_rate':
+                                           ('uniform', (0.5, 0.8)),
+                                           'ntrees': ('choice', [10, 20, 30])})
+        assert set(model.model_params.keys()) == set(['metric', 'ntrees',
+                                                      'col_sample_rate'])
+
+        # Removing a certain parameter
+        model.rem_optimization_parameters('col_sample_rate')
+        assert set(model.model_params.keys()) == set(['metric', 'ntrees'])
