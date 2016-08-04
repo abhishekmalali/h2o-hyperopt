11f3020316ec47e36c380c1815ebbed0d6f271bd
Abhishek Malali <abhishekmalali@gmail.com>
2016-08-04 11:24:34 -0700

    Adding comments to ModelOptimizer

--- h2ohyperopt/modeloptimizer.py
+++ h2ohyperopt/modeloptimizer.py
@@ -5,24 +5,18 @@ from h2o.h2o import H2OGradientBoostingEstimator
 class ModelOptimizer():
 
     def __init__(self, metric=None):
-
+        """Initializes the ModelOptimizer class. Metric is optional argument"""
         self.def_params = {'metric': metric}
         self.model_params = None
         self.model = None
         self.ensemble_model = None
 
     def check_if_metric_set(self):
-        """
-        Function used to check whether the user has defined the error metric
-        """
-        # assert self.def_params['metric'] != None,\
-        # 'GBM requires the model to have a metric. Use function set_metric()'
+        """Function used to check whether the user has defined the error metric"""
         return self.def_params['metric'] != None
 
     def set_metric(self, metric):
-        """
-        Function used to explicitly set the model evaluation metric
-        """
+        """Function used to explicitly set the model evaluation metric"""
         self.def_params['metric'] = metric
         if self.model_params is not None:
             self.model_params['metric'] = metric
@@ -79,7 +73,7 @@ class ModelOptimizer():
         else:
             return self.model_params
 
-    def select_optimization_parameters(self, params, use_default=True):
+    def select_optimization_parameters(self, params):
         """
         Function to select the default parameters
         This function can handle inputs as a list as well as a dictionary
@@ -113,6 +107,11 @@ class ModelOptimizer():
         return
 
     def add_optimization_parameters(self, params):
+        """
+        Add a variable to the optimization variables dictionary
+        Can add default values and custom values
+        Input to be in the form of a dictionary
+        """
         for key in params.keys():
             if params[key] == "Default":
                 self.model_params[key] = self.def_params[key]
@@ -123,8 +122,8 @@ class ModelOptimizer():
 
     def rem_optimization_parameters(self, params):
         """
-        Remove a variable from optimization variables dictionary
-        Can take input as a list or a specific single variable
+        Remove a variable from optimization variables dictionary.
+        Can take input as a list or a specific single variable.
         """
         if type(params) == list:
             for p in params:
@@ -145,10 +144,14 @@ class ModelOptimizer():
     # This is done to ensure format matches whats required for hyperopt
 
     def _select_dist_function(self, name, values):
+        """
+        Internal function to match distribution function in util
+        with the argument for model parameters.
+        """
         return getattr(util, values[0])(name, values[1])
 
     def _create_hyperopt_format(self):
-        # Process self.model_params into hyperopt variables
+        """ Internal function to process self.model_params into hyperopt variables """
         self._hp_model_params = {}
         for key in self.model_params.keys():
             if type(self.model_params[key]) == tuple:
@@ -162,6 +165,17 @@ class ModelOptimizer():
     def start_optimization(self, num_evals=100, trainingFr=None,
                            validationFr=None, predictors=None,
                            response=None):
+        """
+        Function to start training models and optimize over the search space.
+
+        Input
+        ---------------------
+        num_evals: Number of objective function evaluations.
+        trainingFr: H2OFrame with trainig data.
+        validationFr: H2OFrame with valdiation data.
+        predictors: List of column names to be designated predictor columns.
+        response: String indicating the response variable in the trainingFr.
+        """
         self.trials = Trials()
         self.trainFr = trainingFr
         self.validFr = validationFr
@@ -175,9 +189,7 @@ class ModelOptimizer():
         self.optimized = True
 
     def return_best_model(self):
-        """
-        Return the best model
-        """
+        """Return the best model after optimizing """
         if self.optimized is True:
             return self.best_model
         else:
@@ -189,7 +201,7 @@ class ModelOptimizer():
         and validation H2OFrames.
         Input
         ----------------
-        return_value: If True function returns model
+        return_value: If True function returns model.
         """
         if self.best_model is None:
             raise ValueError, "Model not yet optimized"
@@ -208,12 +220,14 @@ class ModelOptimizer():
                         "Validation Score": valScore}
 
     def best_model_test_scores(self, testFr):
+        """Function to generate scores on test data """
         testScore = gen_metric(self.best_model.
                                model_performance(testFr),
                                self._hp_model_params['metric'])
         return testScore
 
     def _gen_score(self, params, model, metric):
+        """ Internal function to generate the score once the model is trained """
         # Checking if the user decided to use cross-validation
         if 'nfolds' in params.keys():
             cross_val_data = model.cross_validation_metrics_summary().\
@@ -230,19 +244,31 @@ class ModelOptimizer():
         return score
 
     def best_model_parameters(self):
-        # Return parameter of the best model discovered
+        """Return parameters of the best model"""
         if self.best_model is not None:
             return self.trials.best_trial['result']['params']
         else:
             raise ValueError, 'Model not yet optimized'
 
     def save_best_model(self, path=None):
+        """ Save the model at a particular path"""
         if self.best_model is not None:
             h2o.save_model(self.best_model, path)
         else:
             raise ValueError, 'Model not yet optimized'
 
     def create_ensembler_data(self, modelList, data, train=True):
+        """
+        Internal function to create ensembling data
+        Can be used to creating training data for ensembling model and for
+        creating data in order to generate test predictions.
+
+        Input
+        ---------------------
+        modelList: List of models to be ensemble_model.
+        data: data to be used to create the ensemble_model.
+        train: Flag to check if a response variable is needed.
+        """
         newFrame = None
         for model in modelList:
             if newFrame is None:
@@ -254,6 +280,14 @@ class ModelOptimizer():
         return newFrame
 
     def best_model_ensemble(self, num_models=3):
+        """
+        Function to create ensembles from the best models during a certain
+        optimization run.
+
+        Input
+        ---------------------
+        num_models: Number of models to be included in the ensemble.
+        """
         scores = []
         for i in range(len(self.trials.trials)):
             scores.append(self.trials.trials[i]['result']['loss'])
@@ -264,12 +298,20 @@ class ModelOptimizer():
         self.create_ensembler_model(modelList)
 
     def create_ensembler_model(self, modelList):
+        """
+        Function to create the ensembles from a list of models.
+        The ensembling model is currently fixed as a H2OGBM.
+
+        Input
+        ---------------------
+        modelList: List of models to create ensemble.
+        """
         self.ensemble_model_list = modelList
         ensembleTrainFr = self.create_ensembler_data(modelList,
                                                      self.trainFr)
         self.ensemble_model = H2OGradientBoostingEstimator(ntrees=200,
-                                                               max_depth=5,
-                                                               nfolds=5)
+                                                           max_depth=5,
+                                                           nfolds=5)
         ensemble_predictors = ensembleTrainFr.columns
         print ensembleTrainFr.columns
         ensemble_predictors.remove(self.response)
@@ -279,7 +321,13 @@ class ModelOptimizer():
 
         print "Model Trained"
 
-
     def predict_ensemble(self, data):
+        """
+        Function to predict from the ensemble model.
+
+        Input
+        ---------------------
+        data: H2OFrame with the test data.
+        """
         dataEn = self.create_ensembler_data(self.ensemble_model_list, data, train=False)
         return self.ensemble_model.predict(dataEn)
