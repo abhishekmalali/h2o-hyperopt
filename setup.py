cbe9217f4ccb357c3fff40a270040a4a10ab901c
Abhishek Malali <abhishekmalali@gmail.com>
2016-08-04 13:53:38 -0700

    Updating setup for Ipython import issues

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 import os
 import logging
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='h2ohyperopt',
       version='0.1',
-      description='Library for Hyperopt based optmization in H2O Ecosystem',
+      description='Library for Hyperopt based optimization in H2O Ecosystem',
       url='http://github.com/abhishekmalali/h2o-hyperopt/',
       author='Abhishek Malali',
       author_email='anon@anon.com',
       license='MIT',
       include_package_data=True,
+      packages=find_packages(),
       install_requires=['numpy', 'scipy', 'nose', 'hyperopt', 'h2o'])
